99c69fc76cbdfdd53b906ccef664c9b6311e2f12
aasuyadav1 <aasuy284@gmail.com>
2025-01-20 21:27:29 +0530

    fix: calendar modal

--- src/app/location/[locationId]/calendar/page.js
+++ src/app/location/[locationId]/calendar/page.js
@@ -1,6 +1,5 @@
 "use client";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchCalendarData_Action } from "@/app/redux/calendar_Slice";
 import {
   Dialog,
   DialogContent,
@@ -16,6 +15,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import axiosInstance from "@/utils/axiosInstance";
+import { useToast } from "@/hooks/use-toast";
 
 // pages/calendar.js or a component file
 import FullCalendar from "@fullcalendar/react";
@@ -25,16 +25,13 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
-import axios from "axios";
-
 import Header from "@/components/Header";
 
 import { usePathname } from "next/navigation";
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import Footer_Component from "@/components/Footer";
 import { getAccessToken } from "@/utils/auth";
 import { jwtDecode } from "jwt-decode";
-import { convertBookingsToEvents } from "@/function";
 
 export default function CalendarPage() {
   const dispatch = useDispatch();
@@ -44,6 +41,9 @@ export default function CalendarPage() {
   const locationName = pathName.split("/")[3];
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [events, setEvents] = useState([]);
+  const toast = useToast();
+  const [allData, setAllData] = useState(null);
+  const [clickedDateData, setClickedDateData] = useState(null);
 
   const timeRanges = {
     afternoon: { start: "12:00:00", end: "15:00:00" },
@@ -52,22 +52,20 @@ export default function CalendarPage() {
 
   const handleDateClick = async (info) => {
     const selectedDate = info.dateStr;
-    try {
-      await dispatch(
-        fetchCalendarData_Action({
-          locationId,
-          date: selectedDate,
-        })
-      ).unwrap();
-      setIsDialogOpen(true);
-    } catch (error) {
-      console.error(error);
-      toast({
-        variant: "destructive",
-        title: "Error",
-        description: error.message || "Failed to fetch calendar data",
-      });
+    const filterByDate = allData.filter((event) => event.date === selectedDate);
+
+    if (filterByDate.length > 0) {
+      const venuesArray = Object.values(filterByDate[0].venues);
+      const transformedData = {
+        ...filterByDate[0],
+        venues: venuesArray,
+      };
+      setClickedDateData([transformedData]);
+    } else {
+      setClickedDateData([]);
     }
+
+    setIsDialogOpen(true);
   };
 
   const transformVenueData = (data, timeRanges) => {
@@ -106,6 +104,8 @@ export default function CalendarPage() {
         `/master-calender-management/locations/${locationId}/?year=${currentYear}&month=${currentMonth}`
       );
 
+      setAllData(response.data.days);
+
       const transformedData = transformVenueData(
         response.data.days,
         timeRanges
@@ -116,6 +116,10 @@ export default function CalendarPage() {
       console.log("the error on fetching calendar data", error);
     }
   };
+
+  useEffect(() => {
+    console.log("clicked date data", clickedDateData);
+  }, [clickedDateData]);
 
   const token = getAccessToken();
   const decodedToken = jwtDecode(token);
@@ -130,8 +134,8 @@ export default function CalendarPage() {
         </div>
         <div className="w-full max-w-7xl bg-white shadow-xl rounded-lg overflow-hidden p-6">
           <FullCalendar
-          eventClassNames="rounded-md py-1 px-2 shadow-sm"
-          dayCellClassNames={"p-5"}
+            eventClassNames="rounded-md py-1 px-2 shadow-sm"
+            dayCellClassNames={"p-5"}
             plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
             initialView="dayGridMonth"
             events={events}
@@ -151,50 +155,53 @@ export default function CalendarPage() {
             <DialogContent>
               <DialogHeader>
                 <DialogTitle>Events and Bookings</DialogTitle>
-                <DialogDescription>Date: {calendarData.date}</DialogDescription>
+                <DialogDescription>
+                  Date: {clickedDateData && clickedDateData[0]?.date}
+                </DialogDescription>
               </DialogHeader>
               {isLoading ? (
                 <div>Loading...</div>
               ) : (
                 <Accordion type="single" collapsible className="w-full">
-                  {calendarData?.venues?.map((venue, index) => (
-                    <AccordionItem value={index + 1} key={index}>
-                      <AccordionTrigger>{venue.venue_name}</AccordionTrigger>
-                      <AccordionContent>
-                        {Object.entries(venue.slots).map(
-                          ([slotName, slotDetails]) => (
-                            <div
-                              key={slotName}
-                              className="p-4 bg-blue-200 border rounded-lg space-y-2 mt-3"
-                            >
-                              <h3 className="text-lg font-bold">
-                                {slotName.charAt(0).toUpperCase() +
-                                  slotName.slice(1)}{" "}
-                                Slot
-                              </h3>
-                              {slotDetails ? (
-                                <div className="flex flex-col gap-2">
-                                  <div className="flex w-full items-center justify-between">
-                                    <p className="text-sm  font-medium text-blue-700">
-                                      Booking Number:{" "}
-                                      {slotDetails.booking_number}
+                  {clickedDateData &&
+                    clickedDateData[0]?.venues?.map((venue, index) => (
+                      <AccordionItem value={index + 1} key={index}>
+                        <AccordionTrigger>{venue.venue_name}</AccordionTrigger>
+                        <AccordionContent>
+                          {Object.entries(venue.slots).map(
+                            ([slotName, slotDetails]) => (
+                              <div
+                                key={slotName}
+                                className="p-4 bg-blue-200 border rounded-lg space-y-2 mt-3"
+                              >
+                                <h3 className="text-lg font-bold">
+                                  {slotName.charAt(0).toUpperCase() +
+                                    slotName.slice(1)}{" "}
+                                  Slot
+                                </h3>
+                                {slotDetails ? (
+                                  <div className="flex flex-col gap-2">
+                                    <div className="flex w-full items-center justify-between">
+                                      <p className="text-sm  font-medium text-blue-700">
+                                        Booking Number:{" "}
+                                        {slotDetails.booking_number}
+                                      </p>
+                                      <p>Lead Name: {slotDetails.lead_name}</p>
+                                    </div>
+                                    <p className="font-bold text-md">
+                                      Occasion: {slotDetails.occasion}
                                     </p>
-                                    <p>Lead Name: {slotDetails.lead_name}</p>
+                                    <p>Mobile: {slotDetails.mobile}</p>
                                   </div>
-                                  <p className="font-bold text-md">
-                                    Occasion: {slotDetails.occasion}
-                                  </p>
-                                  <p>Mobile: {slotDetails.mobile}</p>
-                                </div>
-                              ) : (
-                                <p>No booking available.</p>
-                              )}
-                            </div>
-                          )
-                        )}
-                      </AccordionContent>
-                    </AccordionItem>
-                  ))}
+                                ) : (
+                                  <p>No booking available.</p>
+                                )}
+                              </div>
+                            )
+                          )}
+                        </AccordionContent>
+                      </AccordionItem>
+                    ))}
                 </Accordion>
               )}
             </DialogContent>
